75a8e84131d5d1bd90b24df4d456f3817c3dc9b4
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2021-06-27 21:11:30 +0700

    Initial Commit

---
+++ Sources/VNDTextField/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  InputTextField
+//
+//  Created by Quang Công on 26/06/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static var PRICE_LOCATION = "vn_VN"
+    static var SYMBOL = "đ"
+    static var BUTTON_CORNER_RADIUS: CGFloat = 5
+    static var background: UIColor = .gray
+}
